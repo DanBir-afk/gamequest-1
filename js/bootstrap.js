0a5fb5ebad001895e58c5b03f205722fbd53be9b
d.b.o.c <danielgxk5417@gmail.com>
2026-06-14 01:22:24 +0000

    Delete js directory

--- js/bootstrap.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (!window.__gqParts) window.__gqParts = [];
-    window.__gqParts.forEach(function(part) {
-        try { part(); } catch (e) { console.error('GameQuest module error:', e); }
-    });
-});
